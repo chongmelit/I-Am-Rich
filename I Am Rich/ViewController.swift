7c25b10e60e7af604377cc1f10b8ca2da233be84
chongmelit <chongmelit@163.com>
2020-08-08 16:57:52 +0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by CHONG LIU on 8/8/20.
+//  Copyright © 2020 ChongMelit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
