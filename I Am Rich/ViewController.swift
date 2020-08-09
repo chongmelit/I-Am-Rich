c1f91f3492c63db5254d0dbc68f92f43d8596b91
chongmelit <chongmelit@163.com>
2020-08-10 07:54:04 +0800

    An I Am Rich App with tap gesture recogniser on the image.

--- I Am Rich/ViewController.swift
+++ I Am Rich/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var centerImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        singleTap.numberOfTapsRequired = 1
+        singleTap.numberOfTouchesRequired = 1
+        centerImageView.addGestureRecognizer(singleTap)
     }
+    
+    @objc func imageTapped() {
+        if titleLabel.text == "Monopoly" {
+            titleLabel.text = "Amex Platinum"
+            centerImageView.image = UIImage(named: "Amex Platinum")
+        } else {
+            titleLabel.text = "Monopoly"
+            centerImageView.image = UIImage(named: "Monopoly")
+        }
+    }
+    
 
 
 }
